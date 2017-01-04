06adae13843cde2c0002a1ec415ebab6f4e25034
Veronika <veronika.kotovich@gmail.com>
2017-01-04 15:51:17 +0300

    [MIG] migrate to 10.0

--- connector_prestashop/models/product_template/common.py
+++ connector_prestashop/models/product_template/common.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from openerp import api, fields, models
-from openerp.addons.decimal_precision import decimal_precision as dp
+from odoo.addons import decimal_precision as dp
 
 from ...unit.backend_adapter import GenericAdapter
 from ...backend import prestashop
